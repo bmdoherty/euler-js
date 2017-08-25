99e468cda6490b3c3b24e6841441290f3058d219
Brendan Doherty <github@sidewinder.co.uk>
2017-08-25 15:37:42 +0100

    6: Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.

---
+++ 6.js
@@ -0,0 +1,26 @@
+
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+const range = (min, max) => {
+    let arr = []
+
+    for(let i = min; i <= max; i++){
+        arr.push(i)
+    }
+    return arr
+}
+
+const f = (min,max) => {
+     
+    sumOfSquares = range(min,max).map( n => n * n)
+        .reduce( (sum,value) => sum = sum + value )
+
+    squareOfSums = Math.pow(range(min,max).reduce( (sum,value) => sum = sum + value ),2)
+
+    console.log(squareOfSums)
+    //.map( n => n * n)
+
+    return squareOfSums - sumOfSquares
+}
+
+module.exports = f;
---
+++ 6.test.js
@@ -0,0 +1,12 @@
+const f = require('./6');
+
+test('difference between the sum of the squares of the first ten natural numbers and the square of the sum', () => {
+  expect( f(1,10) ).toBe( 2640 );
+});
+
+test('difference between the sum of the squares of the first 100 natural numbers and the square of the sum', () => {
+    expect( f(1,100) ).toBe( 232792560 );
+});
+
+  
+  
