deadc5fbf2b95f15010c0af52c81e3dd9b120d4f
Brendan Doherty <github@sidewinder.co.uk>
2017-08-26 15:32:45 +0100

    11: Largest product in a grid

--- 11.test.js
+++ 11.test.js
@@ -1,12 +1,9 @@
 const f = require('./11');
 
-test('longest diagonal', () => {
-  expect( f('d') ).toBe( 1788696 );
+test('largest number', () => {
+    expect( f(1) ).toBe( 99 );
+  });
+
+test('largest 4 adjacent line product', () => {
+  expect( f(4) ).toBe( 70600674 );
 });
-
-// test('The sum of the primes below 2 million', () => {
-//     expect( f(2000000) ).toBe( 142913828922 );
-// });
-
-  
-  
