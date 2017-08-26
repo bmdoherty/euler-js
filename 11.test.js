8fac51b537c9a9c225848deef062742f0bb18640
Brendan Doherty <github@sidewinder.co.uk>
2017-08-26 11:47:41 +0100

    jest test  only changed files

---
+++ 11.test.js
@@ -0,0 +1,12 @@
+const f = require('./11');
+
+test('longest diagonal', () => {
+  expect( f('d') ).toBe( 1788696 );
+});
+
+// test('The sum of the primes below 2 million', () => {
+//     expect( f(2000000) ).toBe( 142913828922 );
+// });
+
+  
+  
