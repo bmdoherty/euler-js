c92e3192afd3fb59caa1315bedb8bc7f6dcbe2f8
Brendan Doherty <github@sidewinder.co.uk>
2017-08-27 14:15:07 +0100

    14: Longest Collatz sequence

--- 14.test.js
+++ 14.test.js
@@ -1,9 +1,9 @@
 const f = require('./14');
 
 test('longest collatz chain under 20', () => {
-    expect( f(20) ).toBe( 8348422521);
+    expect( f(20) ).toBe( 18 );
 });
 
-// test('sum 100, first 10 digits', () => {
-//     expect( f(100) ).toBe( 5537376230 );
-// });
+test('slongest collatz chain under 20', () => {
+    expect( f(1000000) ).toBe( 837799 );
+});
