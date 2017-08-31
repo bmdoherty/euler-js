f4209c88f3e13ac799a112cd349ec8dc71abd9d1
Brendan Doherty <github@sidewinder.co.uk>
2017-08-31 10:59:19 +0100

    22: Names scores

--- 22.js
+++ 22.js
@@ -1,9 +1,42 @@
+// What is the total of all the name scores in the file? 
+const request = require('request');
 
-// What is the total of all the name scores in the file?
+async function getNames () {
+  return await new Promise((resolve, reject) => {
+     request('https://projecteuler.net/project/resources/p022_names.txt', (err, res, body) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(body);
+    });
+  });
+}
 
-const f = (n) => {
+const sumOfLetters = (sum, value) => {
+    return sum = sum + value.charCodeAt(0) - 64 
+}
+
+const score = (sum, value, index) => {
+    let name = value.split('')
+    let position = index + 1
+    let lettersSum = name.reduce( sumOfLetters, 0 )
+
+    return sum = sum + (position * lettersSum)
+}
+
+const getTotalScore = (arr) => {
+    return arr.reduce( score , 0 )
+}
+
+async function f() {
+    let response = await getNames()
+
+    let names = response.replace(/["]+/g, '').split(',').sort()
+
+    return  getTotalScore(names) 
+}
+
+module.exports = {f, getTotalScore, score, sumOfLetters}
 
 
-    return 10
 
-module.exports = f
--- 22.test.js
+++ 22.test.js
@@ -1,6 +1,18 @@
-const f = require('./22'); 
+const {f, getTotalScore, score, sumOfLetters} = require('./22'); 
 
-test('count', () => {
-    expect( f() ).toEqual( 50 );
+test('sumOfLetters', () => {
+    expect( sumOfLetters( 0, 'C', 0 ) ).toEqual( 3 );
 });
 
+test('score', () => {
+    expect( score( 0, 'COLIN', 0 ) ).toEqual( 53 );
+});
+
+test('getTotalScore', () => {
+    expect( getTotalScore( ['COLIN','COLIN'] ) ).toEqual( 159 );
+});
+
+it('works with async/await', async () => {
+    expect( await f() ).toEqual(871198282);
+});
+  
