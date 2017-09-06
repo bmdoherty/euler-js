6d317eed5658d91cd293f044d782c705b4201880
Brendan Doherty <github@sidewinder.co.uk>
2017-09-06 11:40:31 +0100

    39: Integer right triangles

--- 39.js
+++ 39.js
@@ -1,30 +1,31 @@
 "use strict"
 
 function solutions(p) {
-    let solutions = []
-    let limit = p/2
+  let solutions = []
+  let lowerLimit = p / 6
+  let upperLimit = p / 2
 
-    for (let a=1; a<limit; a++){
-        for (let b=a+1; b+a<p; b = a+1){
-            let c = p-a-b
-            if (a*a + b*b == c*c){
-                solutions.push([a,b,c])
-            }
-        }
+  for (let a = lowerLimit; a < upperLimit; a++) {
+    for (let b = a + 1; b + a < p; b++) {
+      let c = p - a - b
+      if (a * a + b * b == c * c) {
+        solutions.push([a, b, c])
+      }
     }
-    return solutions
+  }
+  return solutions
 }
 
 const f = (limit) => {
-    var max = 0
-    var perimeter = 0
-    for (let i=0; i<=1000; i++){
-        if (solutions(i).length > max){
-            max = solutions(i).length
-            perimeter = i
-        }
+  var max = 0
+  var perimeter = 0
+  for (let i = 0; i <= limit; i++) {
+    if (solutions(i).length > max) {
+      max = solutions(i).length
+      perimeter = i
     }
-    return perimeter
+  }
+  return perimeter
 }
 
 module.exports = f
