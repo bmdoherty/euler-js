58590d7e8a4a19eb6a30a3860a701d71177b591d
Brendan Doherty <github@sidewinder.co.uk>
2017-09-06 13:00:25 +0100

    40: Champernownes constant

---
+++ 41.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const lookup = (limit) => {
+    let s = ''
+    let i = 1
+
+    while( s.length < limit){
+        s = s + `${i}`
+        i++
+    }
+    
+    return s
+}
+
+const f = (a) => {
+    let max = Math.max(...a)
+    let s = lookup(max)
+    
+    let product = a.map( v => parseInt(s.charAt(v-1)) ).reduce ( (s,v) => s = s * v)
+
+  return product
+}
+
+module.exports = f
---
+++ 41.test.js
@@ -0,0 +1,5 @@
+const f = require('./41');
+
+test('find', () => {
+    expect( f([1,2,3,4,5,6,7,8,9]) ).toEqual( 210 );
+});
