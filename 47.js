49e56c6dce4bcaa1f22720e68c26efe7ae13941a
Brendan Doherty <github@sidewinder.co.uk>
2017-09-18 18:46:07 +0100

    47: Distinct primes factors

--- 47.js
+++ 47.js
@@ -1,21 +1,70 @@
 "use strict"
 console.time("timer")
 
-const getAllFactorsFor = (remainder, factors=[]) => {
-    for (let i = 2; i <= remainder; i++) {
-        if ((remainder % i) === 0) {
-            factors.push(i);
-            getAllFactorsFor(remainder / i, factors);
-            break
+const sieve = (n) => {
+    let primes = new Set()
+    let limit = Math.sqrt(n)
+
+    for (let i=1; i<=n; i++) {
+        primes.add(i)
+    }
+
+    primes.delete(1)
+
+    for(let i=2; i<=limit; i++){
+        if( primes.has(i) ){
+            for(let j=i+i; j<=n; j=j+i){
+                primes.delete(j)
+            }
         }
+    }
+
+    return primes
+}
+
+const primes = sieve(700)
+
+const getPrimeFactorsFor = (N) => {
+    let factors = new Set()
+    let pIter = primes.values()
+    let p = pIter.next().value
+
+    while( N >= p*p ){
+        if ( N % p === 0 ) {
+            factors.add(p);
+            N = N / p
+        }
+        else{
+            p = pIter.next().value
+        }
+    }
+
+    if( primes.has(N)){
+        factors.add(N);
     }
     
     return factors;
 }
 
-const f = () => {
+const f = (n) => {
+    let i = 1
+    let distinctPrimeFactors = 0
+    let consecutiveNumbers = []
 
-    return getAllFactorsFor(644)
+    while( consecutiveNumbers.length < n){
+
+        distinctPrimeFactors =  getPrimeFactorsFor(i).size
+        
+        if(distinctPrimeFactors == n){
+            consecutiveNumbers.push(i)
+        }
+        else{
+            consecutiveNumbers = []
+        }
+        i++
+    }
+    
+    return consecutiveNumbers[0]
 }
 
 module.exports = f
--- 47.test.js
+++ 47.test.js
@@ -1,7 +1,13 @@
 const f = require('./47'); 
 
-test('f', () => {
-    expect( f() ).toEqual( 5777 );
-});
+// test('f', () => {
+//     expect( f(2) ).toEqual( 14 );
+// });
 
-  
+// test('f', () => {
+//     expect( f(3) ).toEqual( 644 );
+// });
+
+test('f', () => {
+    expect( f(4) ).toEqual( 134043 );
+});
