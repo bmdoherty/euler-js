84b8373fdf674a4863ea8d714b38bb6083941c1d
Brendan Doherty <github@sidewinder.co.uk>
2017-09-21 17:24:58 +0100

    54: Poker hands

--- 54.js
+++ 54.js
@@ -1,4 +1,6 @@
-// What is the total of all the name scores in the file? 
+"use strict"
+console.time("timer")
+
 const request = require('request');
 
 async function getHands () {
@@ -12,13 +14,194 @@ async function getHands () {
   });
 }
 
+const HighCard = 1      // Highest value card.
+const OnePair = 2       // Two cards of the same value.
+const TwoPairs = 3      // Two different pairs.
+const ThreeOfAKind = 4  // Three cards of the same value.
+const Straight = 5      // All cards are consecutive values.
+const Flush = 6         // All cards of the same suit.
+const FullHouse = 7     // Three of a kind and a pair.
+const FourOfAKind = 8   // Four cards of the same value.
+const StraightFlush = 9 // All cards are consecutive values of same suit.
+//const RoyalFlush = 10   // Ten, Jack, Queen, King, Ace, in same suit.
 
+const cards = {}
+cards['2'] = 2
+cards['3'] = 3
+cards['4'] = 4
+cards['5'] = 5
+cards['6'] = 6
+cards['7'] = 7
+cards['8'] = 8
+cards['9'] = 9
+cards['T'] = 10
+cards['J'] = 11
+cards['Q'] = 12
+cards['K'] = 13
+cards['A'] = 14
+
+const winner = (p1, p2) => {
+    if(p1.hand > p2.hand){
+        return 'p1'
+    }
+    if(p1.hand < p2.hand){
+        return 'p2'
+    }    
+
+    for(let i=0; i<p1.kicker.length; i++){
+        if( cards[p1.kicker[i]] > cards[p2.kicker[i]]){
+            return 'p1'
+        }
+        if( cards[p1.kicker[i]] < cards[p2.kicker[i]]){
+            return 'p2'
+        }        
+    }
+
+    return 
+}
+const playHand = (hand) => {
+    let handArray = hand.split(' ')
+    let p1 = identifyHand(handArray.slice(0,5))
+    let p2 = identifyHand(handArray.slice(5))
+
+    return winner(p1, p2)
+}
+
+const getValues = (v,i,a) => {
+    return v[0]
+} 
+
+const getSuits = (v,i,a) => {
+    return v[1]
+} 
+
+const orderValues = (values, acesHigh) => {
+    let pairs = []
+    let triplets = []
+    let quads = []
+
+    let rest = values.filter( (v,i,a) => {
+        let duplicateCount = (a.join('').match(new RegExp(v, "g")) || []).length
+        
+        if(duplicateCount == 2){
+            pairs.push( v )
+        }
+        if(duplicateCount == 3){
+            triplets.push( v )
+        }   
+        if(duplicateCount == 4){
+            quads.push( v )
+        }          
+        
+        return duplicateCount == 1
+    })
+    
+    pairs.sort( (a,b) => cards[b] - cards[a])
+    rest.sort( (a,b) => cards[b] - cards[a])
+
+    if(!acesHigh){
+        rest.shift()
+        rest.push("A")
+    }
+
+    return quads.concat(triplets).concat(pairs).concat(rest)
+}
+
+const getDuplicates = (values) => {
+    let result = new Set()
+
+    values.forEach( (v,i) => {
+        if( values.indexOf(v, i+1) > -1){
+            result.add(v)
+        }
+    })
+    return result
+}
+
+const straight = (values) => {
+    let straight = false
+    let acesHigh = true
+    let sorted = values.sort( (a,b) => cards[b] - cards[a]).join('')
+    const acesHighStraightCheck = "AKQJT98765432"
+    const acesLowStraightCheck = "A5432"
+
+    if(acesHighStraightCheck.indexOf(sorted) > -1){
+        straight = true
+    }
+
+    if(acesLowStraightCheck.indexOf(sorted) > -1){
+        straight = true
+        acesHigh = false
+    }
+
+    return [straight, acesHigh]
+}
+
+const identifyHand = (a) => {
+    // hand properties
+    let values = a.map(getValues)
+    let suits = a.map(getSuits)
+    let [isStraight, acesHigh] = straight(values)
+    let distinctValues = new Set(values)
+    let kicker = orderValues(values, acesHigh)
+    let highestValue = kicker[0]
+    let sameSuit = new Set(suits).size == 1
+    let duplicates = getDuplicates(kicker)
+    let hand
+
+    switch (distinctValues.size) {
+        case 2:
+            if(duplicates.size > 1){
+                hand = FullHouse
+            }
+            else {
+                hand = FourOfAKind
+            }
+            break;
+        case 3: 
+            if(duplicates.size > 1){
+                hand = TwoPairs                           
+            } else {
+                hand = ThreeOfAKind               
+            }
+            break;
+        case 4:
+            hand = OnePair
+            break;            
+        case 5:
+            if(sameSuit){
+                if(isStraight){
+                    //if(highestValue == 'A'){
+                    //    hand = RoyalFlush                      
+                    //} else {
+                        hand = StraightFlush                      
+                    //}
+                } else {
+                    hand = Flush                 
+                }
+            } else {
+                if(isStraight){
+                    hand = Straight
+                } else {
+                    hand = HighCard
+                }
+            }
+            break;    
+        default:
+            hand = HighCard
+            break;
+    }
+
+    return {'hand':hand, 'kicker':kicker}
+}
 
 async function f() {
     let response = await getHands()
-    //let names = response.replace(/["]+/g, '').split(',').sort()
+    let hands = response.split(/\r?\n/)
+    let winners = hands.map( playHand )
 
-    return  true //getTotalScore(names) 
+    return  winners.filter( v => v == 'p1').map( v => 1).reduce( (s,v) => s = s+v)
 }
 
-module.exports = {f, getTotalScore, score, sumOfLetters}
+module.exports = {f, identifyHand, winner, playHand}
+console.timeEnd("timer")
--- 54.test.js
+++ 54.test.js
@@ -1,18 +1,121 @@
-const {f, getTotalScore, score, sumOfLetters} = require('./22'); 
+const {f, identifyHand, winner, playHand} = require('./54'); 
 
-test('sumOfLetters', () => {
-    expect( sumOfLetters( 0, 'C', 0 ) ).toEqual( 3 );
+test('identifyHand high card', () => {
+    expect( identifyHand(['7C', '8S', 'KC', '9H', '3S']) ).toEqual({'hand':1, 'kicker':["K", "9", "8", "7", "3"]});
 });
 
-test('score', () => {
-    expect( score( 0, 'COLIN', 0 ) ).toEqual( 53 );
+test('identifyHand one pair', () => {
+    expect( identifyHand(['8C', '8S', 'KC', '9H', '4S']) ).toEqual({'hand':2, 'kicker':["8", "8", "K", "9", "4"]});
 });
 
-test('getTotalScore', () => {
-    expect( getTotalScore( ['COLIN','COLIN'] ) ).toEqual( 159 );
+test('identifyHand two pair', () => {
+    expect( identifyHand(['8C', '8S', 'KC', 'KH', '4S']) ).toEqual({'hand':3, 'kicker':["K", "K", "8", "8", "4"]});
+});
+
+test('identifyHand three of kind', () => {
+    expect( identifyHand(['8C', '8S', 'KC', '8H', '4S']) ).toEqual({'hand':4, 'kicker':["8", "8", "8", "K", "4"]});
+});
+
+test('identifyHand straight', () => {
+    expect( identifyHand(['2C', '3S', '4C', '5H', '6S']) ).toEqual({'hand':5, 'kicker':["6", "5", "4", "3", "2"]});
+});
+
+test('identifyHand straight aces high', () => {
+    expect( identifyHand(['AC', 'QS', 'KC', 'TH', 'JS']) ).toEqual({'hand':5, 'kicker':["A", "K", "Q", "J", "T"]});
+});
+
+test('identifyHand straight aces low', () => {
+    expect( identifyHand(['AC', '2S', '3C', '5H', '4S']) ).toEqual({'hand':5, 'kicker':["5", "4", "3", "2", "A"]});
+});
+
+test('identifyHand flush', () => {
+    expect( identifyHand(['2C', 'AC', '7C', 'JC', '4C']) ).toEqual({'hand':6, 'kicker':["A", "J", "7", "4", "2"]});
+});
+
+test('identifyHand full house 1', () => {
+    expect( identifyHand(['2C', '2H', '2S', 'JC', 'JH']) ).toEqual({'hand':7, 'kicker':["2", "2", "2", "J", "J"]});
+});
+
+test('identifyHand full house 2', () => {
+    expect( identifyHand(['2C', '2H', 'JS', 'JC', 'JH']) ).toEqual({'hand':7, 'kicker':["J", "J", "J", "2", "2"]});
+});
+
+test('identifyHand four of kind', () => {
+    expect( identifyHand(['2C', 'JD', 'JS', 'JC', 'JH']) ).toEqual({'hand':8, 'kicker':["J", "J", "J", "J", "2"]});
+});
+
+test('identifyHand straight flush', () => {
+    expect( identifyHand(['2C', '3C', '4C', '5C', '6C']) ).toEqual({'hand':9, 'kicker':["6", "5", "4", "3", "2"]});
+});
+
+test('identifyHand royal flush', () => {
+    expect( identifyHand(['KC', 'JC', 'TC', 'AC', 'QC']) ).toEqual({'hand':9, 'kicker':["A", "K", "Q", "J", "T"]});
+});
+
+test('identifyHand Ace', () => {
+    expect( identifyHand(['5D', '8C', '9S', 'JS', 'AC']) ).toEqual({'hand':1, 'kicker':["A", "J", "9", "8", "5"]});
+});
+
+test('identifyHand pair with kicker 1', () => {
+    expect( identifyHand(['QH', 'QC', '9H', '4D', '6S']) ).toEqual({'hand':2, 'kicker':["Q", "Q", "9", "6", "4"]});
+});
+
+test('identifyHand pair with kicker 2', () => {
+    expect( identifyHand(['QD', 'QS', '3D', '6D', '7H']) ).toEqual({'hand':2, 'kicker':["Q", "Q", "7", "6", "3"]});
+});
+
+test('winner royal flush', () => {
+    let p1 = {'hand':10, 'kicker':['A', 'K', 'Q', 'J', 'T']}
+    let p2 = {'hand':9, 'kicker':['6', '5', '4', '3', '2']}
+    expect( winner(p1,p2) ).toEqual('p1');
+});
+
+test('winner pair with kicker', () => {
+    let p1 = {'hand':2, 'kicker':['Q', 'Q', '9', '6', '4']}
+    let p2 = {'hand':2, 'kicker':['Q', 'Q', '7', '6', '3']}
+    expect( winner(p1,p2) ).toEqual('p1');
+});
+
+test('1: playhand pair 8s beats pair of 5s', () => {
+    let hand = '5H 5C 6S 7S KD 2C 3S 8S 8D TD'
+
+    expect( playHand(hand) ).toEqual('p2');
+});
+
+test('2: playhand Ace beats Queen', () => {
+    let hand = '5D 8C 9S JS AC 2C 5C 7D 8S QH'
+
+    expect( playHand(hand) ).toEqual('p1');
+});
+    
+test('3: playhand flush beats 3 aces', () => {
+    let hand = '2D 9C AS AH AC 3D 6D 7D TD QD'
+
+    expect( playHand(hand) ).toEqual('p2');
+});
+
+test('4: playhand pair of queens 9 beats 7', () => {
+    let hand = '4D 6S 9H QH QC 3D 6D 7H QD QS'
+
+    expect( playHand(hand) ).toEqual('p1');
+});
+
+test('5: playhand full house 3 4s beats fullhouse 3 3s', () => {
+    let hand = '2H 2D 4C 4D 4S 3C 3D 3S 9S 9D'
+
+    expect( playHand(hand) ).toEqual('p1');
+});
+
+test('6: playhand straight aces high beats aces low', () => {
+    let hand = 'AH QD KC JD TS AC 5D 4S 3S 2D'
+
+    expect( playHand(hand) ).toEqual('p1');
 });
 
 it('works with async/await', async () => {
-    expect( await f() ).toEqual(871198282);
+    expect( await f() ).toEqual(376);
 });
-  
+
+
+// const StraightFlush = 9 // All cards are consecutive values of same suit.
+// const RoyalFlush = 10   // Ten, Jack, Queen, King, Ace, in same su
